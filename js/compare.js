6f16c7e66ac562bb9183ec13eb445b243f337c0e
Lionel Besson <meije@meije.(none)>
2009-09-20 19:00:20 +0200

    easy construct user comparison urls from home page

---
+++ js/compare.js
@@ -0,0 +1,25 @@
+var users_to_compare = new Array();
+
+function checkboxClicked(event) {
+  if ($F(this) == null) { // unselected
+    users_to_compare = users_to_compare.without(this.id.substr(4));
+    $(this).up().previous().removeClassName('selected');
+    if (users_to_compare.length == 0) {
+      $('compare_btn').hide();
+    }
+  } else { // selected
+    users_to_compare.push(this.id.substr(4));
+    $(this).up().previous().addClassName('selected');
+    if (users_to_compare.length == 2) {
+      $('compare_btn').show();
+    }
+  }
+}
+
+$$('.rank_chkbox input').each(function(obj){
+  obj.observe('click', checkboxClicked);
+});
+
+Event.observe('compare_btn', 'click', function(event) {
+  window.location.href = '/users/compare?users=' + users_to_compare.join('-');
+});
