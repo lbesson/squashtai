aba4cf99543b2ca48bd068c41376b39f71242177
Lionel Besson <lionel.besson@gmail.com>
2009-09-21 15:24:12 +0200

    image border

--- main.py
+++ main.py
@@ -367,7 +367,7 @@ class ProfileHandler(webapp.RequestHandler):
     # upload image if relevant
     if self.request.get('avatar'):
       try:
-        avatar = images.resize(self.request.get("avatar"), 50, 50)
+        avatar = images.resize(self.request.get("avatar"), 80, 80)
       except images.Error:
         self.redirect('/profile')
         return
